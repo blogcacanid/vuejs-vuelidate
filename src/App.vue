c86d8a67b6b7ed9166c3f0ebec9144ecefc6e07d
rokib <akun.rokib.id@gmail.com>
2019-11-14 09:53:31 +0700

    publish file index

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <img alt="Vue logo" class="img-fluid" src="./assets/logo.png">
+      </div>
+      <div class="col-md-6">
+        <register/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Register from './components/Register.vue'
+export default {
+  name: 'app',
+  components: {
+    Register
+  }
+}
+</script>
